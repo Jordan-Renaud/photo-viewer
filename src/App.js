d15e2e341982b755d09feded017b2f69bbded8e8
jordan-renaud <jordan.dj.renaud@gmail.com>
2022-05-16 16:39:05 +0100

    add photos to display

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import PhotoViewer from "./components/PhotoViewer";
+import { imageUrls } from "./data/ImageUrls";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>React Photo Viewer</h1>
-      <PhotoViewer />
+      <div className="photoContainer">
+        {imageUrls.map((url, index) => (
+          <PhotoViewer imageUrl={url} key={index} />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/PhotoViewer.jsx
+++ src/components/PhotoViewer.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import "./PhotoViewer.css";
 
-export default function PhotoViewer() {
-  return <div> </div>;
+export default function PhotoViewer({ imageUrl }) {
+  return (
+    <div className="PhotoViewer">
+      <img className="photo" src={imageUrl} alt="gg" />
+    </div>
+  );
 }
