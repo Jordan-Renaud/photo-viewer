bb152804baa9a9aabedfa8eaeb1243bfc9e2fada
jordan-renaud <jordan.dj.renaud@gmail.com>
2022-05-17 14:07:40 +0100

    add terrible test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+// test("renders learn react link", () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test("Images don't break", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const photos = screen.queryAllByRole("img");
+
+  photos.forEach((photo) => {
+    if (!fireEvent.load(photo)) {
+      console.log("Not loading src: ", photo.src);
+    }
+    expect(fireEvent.load(photo)).toBe(true);
+    // console.log(typeof photo);
+    // photo.onerror = console.log;
+    // fireEvent.load(photo);
+  });
 });
--- src/data/ImageUrls.jsx
+++ src/data/ImageUrls.jsx
@@ -4,10 +4,10 @@ function getImageUrls() {
   const urls = [];
 
   for (let i = 0; i < 50; i++) {
-    if (!brokenImages.includes(i)) {
-      const imageNumberString = i.toString().padStart(2, "0");
-      urls.push(`https://picsum.photos/id/6${imageNumberString}/1600/900.jpg`);
-    }
+    // if (!brokenImages.includes(i)) {
+    const imageNumberString = i.toString().padStart(2, "0");
+    urls.push(`https://picsum.photos/id/6${imageNumberString}/1600/900.jpg`);
+    // }
   }
 
   return urls;
