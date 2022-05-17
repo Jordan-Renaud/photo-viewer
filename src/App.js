b775b23f713d0b44b732c9fcd99111df1e9adde7
jordan-renaud <jordan.dj.renaud@gmail.com>
2022-05-17 09:58:20 +0100

    add selectable large image

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import { useState } from "react";
 import PhotoViewer from "./components/PhotoViewer";
 import { imageUrls } from "./data/ImageUrls";
 import "./App.css";
 
 function App() {
+  const [selectedUrl, setSelectedUrl] = useState(imageUrls[0]);
+
   return (
     <div className="App">
       <h1>React Photo Viewer</h1>
+      <img className="largeImage" src={selectedUrl} alt="gg" />
       <div className="photoContainer">
         {imageUrls.map((url, index) => (
-          <PhotoViewer imageUrl={url} key={index} />
+          <PhotoViewer
+            imageUrl={url}
+            key={index}
+            setSelectedUrl={setSelectedUrl}
+          />
         ))}
       </div>
     </div>
--- src/components/PhotoViewer.jsx
+++ src/components/PhotoViewer.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "./PhotoViewer.css";
 
-export default function PhotoViewer({ imageUrl }) {
+export default function PhotoViewer({ imageUrl, setSelectedUrl }) {
   return (
     <div className="PhotoViewer">
-      <img className="photo" src={imageUrl} alt="gg" />
+      <img
+        className="photoSmall"
+        src={imageUrl}
+        alt="gg"
+        onClick={() => setSelectedUrl(imageUrl)}
+      />
     </div>
   );
 }
